084ebd0420d354d591c2a654281d3adc5f79ab02
alexey <selex1995@yandex.ru>
2023-04-22 10:44:59 +0300

    add main-func

---
+++ Cakeshop/Euphoria.py
@@ -0,0 +1,25 @@
+def main():
+    print("""
+Добро пожаловать в кондитерскую!
+
+Выберите свою роль:
+    
+1 - Покупатель;
+2 - Администратор;
+3 - Поставщик
+""")
+
+    role_choice = input("Ваш выбор: ").lower()
+
+    if role_choice == '1' or role_choice == 'покупатель':
+        print('Привет, покупатель!')
+
+    elif role_choice == '2' or role_choice == 'администратор':
+        print('Привет, администратор!')
+
+    elif role_choice == '3' or role_choice == 'поставщик':
+        print('Привет, поставщик!')
+
+
+if __name__ == '__main__':
+    main()
