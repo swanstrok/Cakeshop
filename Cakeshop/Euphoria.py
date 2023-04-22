527298e7f1448deed878f632b6235b511061a57b
alexey <selex1995@yandex.ru>
2023-04-22 12:49:40 +0300

    add cleint, cateter, admin roleviews-templates for Euphoria

--- Cakeshop/Euphoria.py
+++ Cakeshop/Euphoria.py
@@ -3,10 +3,34 @@ import json
 
 def documentation(filename: str) -> dict:
     """Функция чтения документации из JSON и создания в ее в виде словаря"""
-    with open(filename) as f:
-        docs = json.load(f)
+    # with open(filename) as f:
+    #     docs = json.load(f)
+
+    try:
+        file = open(file=filename, mode='r')
+        docs = json.load(file)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        file = open(file=filename, mode='w')
+        docs = {}
+
+    file.close()
 
     return docs
+
+
+def admin(production: dict):
+    """Роль админа"""
+    pass
+
+
+def client(production: dict):
+    """Роль покупателя"""
+    pass
+
+
+def cateter(production: dict):
+    """Роль поставщика"""
+    pass
 
 
 def main(production: dict):
@@ -24,18 +48,16 @@ def main(production: dict):
 
     if role_choice == '1' or role_choice == 'покупатель':
         print('Привет, покупатель!')
+        client(production)
 
     elif role_choice == '2' or role_choice == 'администратор':
         print('Привет, администратор!')
+        admin(production)
 
     elif role_choice == '3' or role_choice == 'поставщик':
         print('Привет, поставщик!')
+        cateter(production)
 
 
 if __name__ == '__main__':
-    try:
-        production = documentation('production.json')
-    except FileNotFoundError:
-        production = open(file='production.json', mode='w')
-        production.close()
-    main(production)
+    main(documentation('production.json'))
